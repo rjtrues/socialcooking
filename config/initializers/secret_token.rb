54bb1cdfa9bd1d09cb11f6e9c240a2fa088a6084
rtruesdell <rjtrues@gmail.com>
2012-01-29 17:48:44 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialcooking::Application.config.secret_token = '35a38c4b6b06c6ce9aa47a099d1d4fb278f39ad476135c5f1b4cd977b9e1c3a4e283f34e812fcd39bd11ceb620d9a8a7dd868028ea32a7d1a68edd0439aa15ca'
