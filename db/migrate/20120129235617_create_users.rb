ba4c453dee4072d113a2bd2abea45f0977f82df9
matt kiswardy <matt.kiswardy@gmail.com>
2012-01-29 18:58:58 -0500

    added user capability

---
+++ db/migrate/20120129235617_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :dob
+      t.string :location
+      t.string :occupation
+      t.string :userlevel
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
